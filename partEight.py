9f64a087efe3a2b1974ef77a53186e05bc408f29
Alex-Bosman <awb31@kent.ac.uk>
2024-10-09 23:40:46 +0100

    progress on game loop

--- partEight.py
+++ partEight.py
@@ -8,33 +8,70 @@
 # list all details in a room
 # commands [take, drop, inventory, look, score, quit, north, south, east, west, yes, no, health]
 
-# room a - starter room [summary: ] [doors to: b, c, d] [items:] [enemies: none] 
+# possible items:
+# weapon, armour, health potion, materials for crafting, score bonus?
+
+
+# room a - starter room [summary: ] [doors to: b, c, d] [items: weapon, material, armour] [enemies: none] 
 # room b -  room [summary: ] [doors to: a] [items:] [enemies:]
 # room c -  room [summary: ] [doors to: a, e] [items:] [enemies:] 
-# room d -  room [summary: ] [doors to: a] [items:] [enemies:] 
+# room d -  room [summary: ] [doors to: a, e] [items:] [enemies:] 
+# room e -  room [summary: ] [doors to: c, d] [items:] [enemies:] 
+# room f -  room [summary: ] [doors to: c, g] [items:] [enemies:] 
+# room g -  room [summary: ] [doors to: f] [items:] [enemies:]
+# room h -  room [summary: ] [doors to: end, f] [items:] [enemies:] 
 
-# room e -  room [summary: ] [doors to: ] [items:] [enemies:] 
-# room f -  room [summary: ] [doors to: ] [items:] [enemies:] 
-# room g -  room [summary: ] [doors to: ] [items:] [enemies:]
-# room h -  room [summary: ] [doors to: end, ] [items:] [enemies:] 
+# room layout
+# _ h _
+# g f _
+# _ c e
+# b a d
 
 # testing
+class textAdventure:
+    def __init__(self):
+        self.inventory = []
+        self.health = 100
+        self.score = 0
+        self.validInputs = ["take", "drop", "inventory", "look", "score", "quit", "north", "south", "east", "west", "yes", "no", "health"]
+
+        self.roomA_items = ["stick", 5]
+        self.roomA_enemies = [("slime", 3, 3)]
+        
+    def inputParser(self, userInput):
+        if userInput in self.validInputs:
+            return True
+        else:
+            return False
 
 
+    def startGame(self):
+        gameRunning = True
+        room = "roomA"
+        print("You are an adventurer who is on a quest to find..")
+        while gameRunning == True:
+            match room:
+                case "roomA":
+                    print("You begin your adventure in a")
+                    userInput = str(input("Input: "))
+                case "roomB":
+                    pass
+                case "roomC":
+                    pass
+                case "roomD":
+                    pass
+                case "roomE":
+                    pass
+                case "roomF":
+                    pass
+                case "roomG":
+                    pass
+                case "roomH":
+                    pass
 
-import tkinter as tk
-from tkinter import ttk
 
-
-class mainWindow:
-    def __init__(self, root):
-        root.geometry("800x600")
-        btn = ttk.Button(root, text="hello")
-        btn.pack()
 
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    w = mainWindow(root)
-    root.mainloop()
+    w = textAdventure()
